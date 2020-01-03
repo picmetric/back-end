b78777252dab4cf238b8136a3c56da85ae6977cf
zacharybaca <baca.zach@icloud.com>
2020-01-03 02:24:32 -0600

    added users migration

--- data/migrations/20191231160759_users_table.js
+++ data/migrations/20191231160759_users_table.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('users', tbl => {
+    tbl.increments();
+    tbl.varchar('name', 255).notNullable();
+    tbl
+      .varchar('username', 255)
+      .notNullable()
+      .unique();
+    tbl.varchar('password', 255).notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('users');
 };
