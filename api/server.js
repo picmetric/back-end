9dd74e3238a7490d0eb84d7a74654a971f09459c
Kevin Smith <keveightysev@gmail.com>
2020-01-07 16:55:01 -0600

    reverting

--- api/server.js
+++ api/server.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const sessions = require('express-session');
-const KnexSessionStore = require('connect-session-knex')(sessions);
+const express = require("express");
+const sessions = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(sessions);
 
-const apiRouter = require('./api-router');
-const signedUrl = require('../images/image-route');
-const configureMiddleware = require('./configure-middleware.js');
-const knex = require('../data/db-config');
+const apiRouter = require("./api-router");
+const signedUrl = require("../images/image-route");
+const configureMiddleware = require("./configure-middleware.js");
+const knex = require("../data/db-config");
 
 const server = express();
 
 const sessionConfiguration = {
-  name: 'users',
-  secret: 'iamuser',
+  name: "users",
+  secret: "iamuser",
   saveUninitialized: true,
   resave: false,
   store: new KnexSessionStore({
     knex,
-    tablename: 'sessions',
+    tablename: "sessions",
     createtable: true,
     clearInterval: 1000 * 60 * 10,
-    sidfieldname: 'sid'
+    sidfieldname: "sid"
   }),
   cookie: {
     maxAge: 1000 * 60 * 10,
@@ -32,8 +32,8 @@ configureMiddleware(server);
 
 server.use(sessions(sessionConfiguration));
 
-server.use('/api', apiRouter);
+server.use("/api", apiRouter);
 
-server.use('/images', signedUrl);
+server.use("/images", signedUrl);
 
 module.exports = server;
